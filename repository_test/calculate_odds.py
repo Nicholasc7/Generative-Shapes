d152e627cd785fedebd451c111628541552f5a22
Nicholas <nicholas98rc@gmail.com>
2023-07-16 13:39:46 -0700

    added welcome message

--- repository_test/calculate_odds.py
+++ repository_test/calculate_odds.py
@@ -7,5 +7,6 @@ for x in range(1,11):
         odd_list.append(x)
     else:
         even_list.append(x)
+print("Welcome to number calculator!")
 print('Odd List:', odd_list)
 print('Even List:', even_list)
